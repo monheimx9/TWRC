162a512545fb784fac262a015c08b22eaa7f0824
Julien Tual <julientual35@gmail.com>
2024-09-15 21:18:09 +0200

    done

--- History/DataReader.js
+++ History/DataReader.js
@@ -1,9 +1,8 @@
-// ---
-// --- Getting the data
-// ---
+///////////////////
+// Getting the data
+///////////////////
 
 // Import data / fetch
-
 const fileUrl = 'https://raw.githubusercontent.com/Loupphok/TWRC/main/data/WRDb.csv'; // Replace with the URL or path to your CSV file
 let csvData = [];
 
@@ -12,15 +11,16 @@ const params = new URLSearchParams(window.location.search);
 
 // Récupérer la valeur de l'ID
 const id = params.get('id');
+
 // Afficher ou utiliser la valeur récupérée
 SelectedMap = id.replace(/_/g, ' ');
 
-// ---
-// --- Output of blocks on the page
-// ---
+
+///////////////////
+// Output of blocks on the page
+///////////////////
 
 // List insertion
-
 document.write('<div class="MapChoiceBlock">')
 
 document.write(createMapSelector());
@@ -28,13 +28,17 @@ document.write('<h1 class="output" id="output1">Map: '+SelectedMap+'</h1>')
 const out1 = document.getElementById("output1"); // Getting what's inside "ouiput1"
 document.write('</div>') // end of List block
 
-
 // Leaderboard insertion
 
 document.write('<div class="LeaderboardBlock">')
 document.write("<table id='Leaderboard'><tr><th colspan='2' class='LeaderboardPlayer' id='headerPlayer'>Player</th><th class='LeaderboardTime'>Time</th><th class='LeaderboardDate'>Date</th><th class='LeaderboardInfo'>Info</th></tr></table>");
 const LBtable = document.getElementById("Leaderboard");
 document.write('</div>') // end of Leaderboard block
+
+
+///////////////////
+// Parse and show data
+///////////////////
 
 Promise.all([
     fetch('https://raw.githubusercontent.com/Loupphok/TWRC/main/data/WRDb.csv'),
@@ -45,15 +49,17 @@ Promise.all([
         return response.text();
     }))
 }).then(function (alldata){
+
     ///////////////////
     // Fetching HISTORY data
     ///////////////////
+
+    // Import data from WRDb.csv
     data = alldata[0];
 
     // Split the file content by newlines to get each row
     const rows = data.split('\n').filter(row => row.trim().length > 0);
 
-    
     // Map through each row and split by comma to get individual columns
     csvData = rows.map(row => row.split(';').filter(cell => cell.trim().length > 0));
     let myArray = csvData;
@@ -63,6 +69,7 @@ Promise.all([
     // Fetching Nation data
     ///////////////////
 
+    // Import data from Nation.csv
     dataNation = alldata[1];
 
     // Split the file content by newlines to get each row
@@ -80,6 +87,7 @@ Promise.all([
     // Fetching Flag data
     ///////////////////
 
+    // Import data from Flag.csv
     dataFlag = alldata[2];
 
     // Split the file content by newlines to get each row
@@ -92,6 +100,10 @@ Promise.all([
     for (elem of csvDataFlag) {
         Flag[elem[0]] = "assets/flags/" + elem[1];
     }
+
+    ///////////////////
+    // Create and show the table
+    ///////////////////
 
     // Setup of the header of the table
     var result = "";
@@ -108,7 +120,6 @@ Promise.all([
             for(var j=0; j<myArray[i].length; j++){
                 if(j == 0){ // Player column
                     // Finding nationality
-                    // result += "<td class='LeaderboardNation'>" + Nation[myArray[i][j]] + "</td>";
                     result += "<td class='LeaderboardNation'>" + '<div class="FlagPic"><img src="' + Flag[Nation[myArray[i][j]]] + '" alt=""></div>' + "</td>";
                     if(Cheat){
                         result += "<td class='LeaderboardPlayer'><span class='Cheated'>"+myArray[i][j]+"</span></td>";
@@ -175,17 +186,19 @@ Promise.all([
             result += "</tr>";
         }
     }
+    // Inputing the loop into the table
     LBtable.innerHTML = result;
  
 }).catch(function (error){
     console.log(error);
 });
 
-    // Adding extra lines in the end to scroll down bellow leaderboard
 
+// Adding extra lines in the end to scroll down bellow leaderboard
 document.write("<p><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></p>")
 document.write("<p><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></p>")
 
+// Function that creates the horizontal scroller on top of the page to chose close relatives maps
 function createMapSelector() {
     code = '';
     code += '<div style="width: 75%; overflow-x: scroll; margin: auto;">';
@@ -200,20 +213,11 @@ function createMapSelector() {
     return code;
 }
 
+// Function that returns a column into an array
 function getUniqueColumn(data, column){
     const Something = new Map()
     for(let i = 0; i < data.length; i++){
         Something.set(data[i][column], "sex");
     }
     return [...Something.keys()]
-
-}
-
-function MapSelector(maps){
-    var result = '<select name="Maps" id="MapSelector" onchange="getSelectValue()">';
-    for(let map of maps){
-        result += '<option value="' + map + '">' + map + '</option>';
-    }
-    result += "</select>";
-    return result;
 }
