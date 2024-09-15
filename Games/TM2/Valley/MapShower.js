e8dde81a22338e8994b432b628972efa5320e067
Julien Tual <julientual35@gmail.com>
2024-09-15 14:41:45 +0200

    upload

---
+++ Games/TM2/Valley/MapShower.js
@@ -0,0 +1,63 @@
+var result = "";
+var letter = "";
+var map = "";
+const envi = "Valley"; //Select the envi
+
+for(pre of [["A", "White"],["B", "Green"],["C", "Blue"],["D", "Red"]]) {
+    letter = pre[0];
+    result += '<div class="ChoiceBox">';
+
+    result += '<div class="FlagHeader"><img src="MapPics/Flag' + letter + '.png">';
+    result += '<h1 class="FlagTitle">' + pre[1] + " difficulty" + '</h1><img src="MapPics/Flag' + letter + '.png">';
+    result += '</div>' // End of class: FlagHeader
+
+    result += '<div class="ChoiceBoxFlag">';
+
+    for(var i=0; i<15; i++){
+        map = letter + (i+1).toString().padStart(2, '0');
+        result += '<div class="MapThumbnailBox" id="' + map + '" onclick="location.href=' + "'" + "../../../History/map.html?id=" + map + "_-_" + envi + "'" + ';" style="cursor: pointer;">';
+        result += '<div class="ThumbnailHeader">';
+        result += '<h3 class="ThumbnailMapName">' + map + '</h3>';
+        result += '</div>' // End of class: ThumbnailHeader
+        result += '<img class="MapThumbnail" src="MapPics/TM2_' + envi + '_' + map + '.jpg"></img>';
+        result += '<div class="ThumbnailFooter">';
+        result += '<h5 class="ThumbnailWrInfo">WR: <span class="Wr">XX:XX.XXX</span> - <img class="WrFlag" src="../../../assets/flags/question.png"><span class="WrHolder"> ________</span></h5>';
+        result += '</div>'; // End of class: ThumbnailFooter
+        result += '</div>'; // End of class: MapThumbnailBox
+    }
+
+    result += '</div>'; // End of class: ChoiceBoxFlag
+
+    result += '</div>'; // End of class: ChoiceBox
+
+}
+
+// E maps
+letter = "E";
+result += '<div class="ChoiceBox">';
+
+result += '<div class="FlagHeader"><img src="MapPics/Flag' + letter + '.png">';
+result += '<h1 class="FlagTitle">' + "Black difficulty" + '</h1><img src="MapPics/Flag' + letter + '.png">';
+result += '</div>' // End of class: FlagHeader
+
+result += '<div class="ChoiceBoxFlag">';
+
+for(var i=0; i<5; i++){
+    map = letter + (i+1).toString().padStart(2, '0');
+    result += '<div class="MapThumbnailBox" id="' + map + '" onclick="location.href=' + "'" + "../../../History/map.html?id=" + map + "_-_" + envi + "'" + ';" style="cursor: pointer;">';
+    result += '<div class="ThumbnailHeader">';
+    result += '<h3 class="ThumbnailMapName">' + map + '</h3>';
+    result += '</div>' // End of class: ThumbnailHeader
+    result += '<img class="MapThumbnail" src="MapPics/TM2_' + envi + '_' + map + '.jpg"></img>';
+    result += '<div class="ThumbnailFooter">';
+    result += '<h5 class="ThumbnailWrInfo">WR: <span class="Wr">X:XX.XXX</span> - <span class="WrHolder">________</span></h5>';
+    result += '</div>'; // End of class: ThumbnailFooter
+    result += '</div>'; // End of class: MapThumbnailBox
+}
+
+result += '</div>'; // End of class: ChoiceBoxFlag
+
+result += '</div>'; // End of class: ChoiceBox
+
+console.log("test")
+document.write(result);
---
+++ Games/TMT/Red/MapShower.js
@@ -0,0 +1,52 @@
+var result = "";
+var FirstMap = 121;
+var map = "";
+var envi = "";
+
+for(var i=FirstMap; i<40+FirstMap; i++){
+    map = i.toString().padStart(3, '0');
+    if(i<10+FirstMap){
+        envi = "Canyon";
+    }
+    else if(i<20+FirstMap){
+        envi = "Valley";
+    }
+
+    else if(i<30+FirstMap){
+        envi = "Lagoon";
+    }
+
+    else {
+        envi = "Stadium";
+    }
+
+    if ((i - 1) % 10 === 0) {
+        result += '<div class="ChoiceBox">';
+
+        result += '<div class="FlagHeader"><img src="MapPics/' + envi + '.png">';
+        result += '<h1 class="FlagTitle">' + envi + '</h1><img src="MapPics/' + envi + '.png">';
+        result += '</div>' // End of class: FlagHeader
+
+        result += '<div class="ChoiceBoxFlag">';
+    }
+
+    result += '<div class="MapThumbnailBox" id="' + map + '" onclick="location.href=' + "'" + "../../../History/map.html?id=" + map  + "'" + ';" style="cursor: pointer;">';
+    result += '<div class="ThumbnailHeader">';
+    result += '<h3 class="ThumbnailMapName">#' + map + '</h3>';
+    result += '</div>' // End of class: ThumbnailHeader
+    result += '<img class="MapThumbnail" src="MapPics/' + map + '.jpg"></img>';
+    result += '<div class="ThumbnailFooter">';
+    result += '<h5 class="ThumbnailWrInfo">WR: <span class="Wr">XX:XX.XXX</span> - <img class="WrFlag" src="../../../assets/flags/question.png"><span class="WrHolder"> ________</span></h5>';
+    result += '</div>'; // End of class: ThumbnailFooter
+    result += '</div>'; // End of class: MapThumbnailBox
+
+
+    if ((i) % 10 === 0) {
+        result += '</div>'; // End of class: ChoiceBoxFlag
+
+        result += '</div>'; // End of class: ChoiceBox
+    }
+
+}
+
+document.write(result);
