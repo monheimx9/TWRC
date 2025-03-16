e1d3135c45f107dea1d59ecae96e86b769feb132
Julien Tual <julientual35@gmail.com>
2025-03-16 18:44:51 +0100

    other tests

--- otherTests/PREVIOUS help/test.py
+++ otherTests/PREVIOUS help/test.py
@@ -1,4 +1,9 @@
-test = "001"
-test = int(test)
-test = str(test)
-print(test)
+map_list = []
+for i in ["A", "B", "C", "D", "E"]:
+    for j in range(15):
+        if j+1 < 10:
+            map_list.append(f"{i}0{j+1} - Stadium")
+        else:
+            map_list.append(f"{i}{j+1} - Stadium")
+
+print(map_list)
